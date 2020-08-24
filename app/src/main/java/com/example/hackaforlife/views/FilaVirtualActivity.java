df5f8a2c7f2ad8971c829b63829f125fa011a12c
GustavoBessa <gustavoux@yahoo.com.br>
2020-08-23 23:47:47 -0300

    Bug fix.

--- app/src/main/java/com/example/hackaforlife/views/FilaVirtualActivity.java
+++ app/src/main/java/com/example/hackaforlife/views/FilaVirtualActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.TextView;
 
 import com.example.hackaforlife.R;
@@ -30,6 +32,7 @@ public class FilaVirtualActivity extends AppCompatActivity {
     private List<FichaCadastro> fichasFila;
     private int tempoAnterior;
     private boolean primeiroTempo;
+    private boolean ocupado = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,71 +53,84 @@ public class FilaVirtualActivity extends AppCompatActivity {
         buscaFila.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                fichasFila = new ArrayList<>();
-                if (dataSnapshot.exists()) {
-                    for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                        FichaCadastro fichaCadastro = new FichaCadastro();
-                        HashMap formMap = (HashMap) snapshot.getValue();
-                        fichaCadastro.setPesoForm((Long) formMap.get("pesoForm"));
-                        fichaCadastro.setCpfPaciente(snapshot.getKey());
-                        fichasFila.add(fichaCadastro);
+                    ocupado = true;
+                    fichasFila = new ArrayList<>();
+                    if (dataSnapshot.exists()) {
+                        for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            FichaCadastro fichaCadastro = new FichaCadastro();
+                            HashMap formMap = (HashMap) snapshot.getValue();
+                            fichaCadastro.setPesoForm((Long) formMap.get("pesoForm"));
+                            fichaCadastro.setCpfPaciente(snapshot.getKey());
+                            fichasFila.add(fichaCadastro);
 
-                    }
-                }
-                Collections.sort(fichasFila, new Comparator<FichaCadastro>() {
-                    @Override
-                    public int compare(FichaCadastro f1, FichaCadastro f2) {
-                        return Long.compare(f2.getPesoForm(), f1.getPesoForm());
-                    }
-                });
-                int tempoEstimado = 0;
-                if(tempoEstimado == 0)
-                for(FichaCadastro f : fichasFila) {
-                        System.out.println(cpfPaciente + f.getCpfPaciente() + " " + f.getPesoForm() + tempoEstimado);
-                    if(!f.getCpfPaciente().equals(cpfPaciente)) {
-                        tempoEstimado += 30;
-                    }
-                    else break;
-                }
-                if(tempoEstimado <= 30) {
-                    Intent i = new Intent(FilaVirtualActivity.this, SuaVezActivity.class);
-                    i.putExtra("nomeUnidadeSaude", nomeUnidadeSaude);
-                    startActivity(i);
-                    finish();
-                } else {
-                    if(tempoEstimado >= 60) {
-                        if(tempoEstimado % 60 == 0) {
-                            mTextField.setText(String.valueOf(tempoEstimado / 60) + " hora(s).");
-                        } else {
-                            int horasEstimadas = 0;
-                            int minsEstimados = 0;
-                            horasEstimadas = tempoEstimado / 60;
-                            minsEstimados = tempoEstimado - horasEstimadas*60;
-                            mTextField.setText(String.valueOf(horasEstimadas) + " hora(s) e "
-                             + String.valueOf(minsEstimados) + " minutos.");
                         }
-                    } else {
-                        mTextField.setText(String.valueOf(tempoEstimado) + " minutos.");
                     }
-                }
-                if(primeiroTempo) {
-                    tempoAnterior = tempoEstimado;
-                    primeiroTempo = false;
-                }
-                else if(tempoEstimado > tempoAnterior) {
-                    mainView.setVisibility(View.INVISIBLE);
-                    notifyView.setVisibility(View.VISIBLE);
-                    new CountDownTimer(2000, 1000) {
-                        public void onFinish() {
-                            mainView.setVisibility(View.VISIBLE);
-                            notifyView.setVisibility(View.INVISIBLE);
+                final Handler handler = new Handler();
+                final int delay = 500; //milliseconds
+                final View semFichasView = findViewById(R.id.frame7);
+                final View loadingView = findViewById(R.id.frame1);
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        if (!fichasFila.isEmpty())//checking if the data is loaded or not
+                        {
+                            Collections.sort(fichasFila, new Comparator<FichaCadastro>() {
+                                @Override
+                                public int compare(FichaCadastro f1, FichaCadastro f2) {
+                                    return Long.compare(f2.getPesoForm(), f1.getPesoForm());
+                                }
+                            });
+                            int tempoEstimado = 0;
+                            for(FichaCadastro f : fichasFila) {
+                                System.out.println(cpfPaciente + f.getCpfPaciente() + " " + f.getPesoForm() + tempoEstimado);
+                                if(!f.getCpfPaciente().equals(cpfPaciente)) {
+                                    tempoEstimado += 30;
+                                }
+                                else break;
+                            }
+                            if(tempoEstimado <= 30) {
+                                Intent i = new Intent(FilaVirtualActivity.this, SuaVezActivity.class);
+                                System.out.println(cpfPaciente +  " "  + tempoEstimado);
+                                i.putExtra("nomeUnidadeSaude", nomeUnidadeSaude);
+                                startActivity(i);
+                                finish();
+                            } else {
+                                if(tempoEstimado >= 60) {
+                                    if(tempoEstimado % 60 == 0) {
+                                        mTextField.setText(String.valueOf(tempoEstimado / 60) + " hora(s).");
+                                    } else {
+                                        int horasEstimadas = 0;
+                                        int minsEstimados = 0;
+                                        horasEstimadas = tempoEstimado / 60;
+                                        minsEstimados = tempoEstimado - horasEstimadas*60;
+                                        mTextField.setText(String.valueOf(horasEstimadas) + " hora(s) e "
+                                                + String.valueOf(minsEstimados) + " minutos.");
+                                    }
+                                } else {
+                                    mTextField.setText(String.valueOf(tempoEstimado) + " minutos.");
+                                }
+                            }
+                            if(primeiroTempo) {
+                                tempoAnterior = tempoEstimado;
+                                primeiroTempo = false;
+                            }
+                            else if(tempoEstimado > tempoAnterior) {
+                                mainView.setVisibility(View.INVISIBLE);
+                                notifyView.setVisibility(View.VISIBLE);
+                                new CountDownTimer(2000, 1000) {
+                                    public void onFinish() {
+                                        mainView.setVisibility(View.VISIBLE);
+                                        notifyView.setVisibility(View.INVISIBLE);
+                                    }
+                                    public void onTick(long millisUntilFinished) {}
+                                }.start();
+                            }
+                            tempoAnterior = tempoEstimado;
                         }
-                        public void onTick(long millisUntilFinished) {}
-                    }.start();
+                    }
+                }, delay);
                 }
-                tempoAnterior = tempoEstimado;
 
-            }
+
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
