57bb81dac605a215e39be910b0361e216663a2ce
GustavoBessa <gustavoux@yahoo.com.br>
2020-08-23 23:27:35 -0300

    Bug fix.

--- app/src/main/java/com/example/hackaforlife/views/SuaVezActivity.java
+++ app/src/main/java/com/example/hackaforlife/views/SuaVezActivity.java
@@ -76,5 +76,6 @@ public class SuaVezActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
+        finish();
     }
 }
